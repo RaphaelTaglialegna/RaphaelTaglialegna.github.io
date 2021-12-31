940942598ad2f797ef663af265ba21032ef0cd2e
Raphael Taglialegna <raphael.trcosta@gmail.com>
2021-12-31 08:25:39 -0300

    Some fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
-import tw from 'twin.macro';
-import TopSection from './containers/topSection/';
-import ProjectsSection from './containers/projects';
-import About from './containers/about';
-import Contact from './containers/contact';
+import "./App.css";
+import tw from "twin.macro";
+import TopSection from "./containers/topSection";
+import ProjectsSection from "./containers/projects";
+import About from "./containers/about";
+import Contact from "./containers/contact";
 
 const  AppContainer = tw.div`
     flex
@@ -12,13 +12,12 @@ const  AppContainer = tw.div`
     h-full
 `;
 function App() {
-    
   return (
     <AppContainer>
-        <TopSection />
-        <ProjectsSection />
-        <About />
-        <Contact />
+      <TopSection />
+      <ProjectsSection />
+      <About />
+      <Contact />
     </AppContainer>
   );
 }
